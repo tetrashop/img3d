9f4dfc954f2a51b90eabb62692e469c1a88bf4a5
Your Name <your@email.com>
2026-05-14 17:24:44 +0330

    پروژه استاندارد Gradle با Gradle Wrapper کامل و اکشن بهینه

--- android-app/app/src/main/java/com/img3d/app/MainActivity.java
+++ android-app/app/src/main/java/com/img3d/app/MainActivity.java
@@ -1,10 +1,8 @@
 package com.img3d.app;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
