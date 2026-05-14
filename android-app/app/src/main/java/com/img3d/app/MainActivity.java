a6cd81efc0dfdca76d58c71a1205466845ac0622
Your Name <your@email.com>
2026-05-14 16:50:01 +0330

    ساخت پروژه Android و workflow ساده برای APK

---
+++ android-app/app/src/main/java/com/img3d/app/MainActivity.java
@@ -0,0 +1,21 @@
+package com.img3d.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getWebSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webView.loadUrl("http://localhost:5000");
+        setContentView(webView);
+    }
+}
